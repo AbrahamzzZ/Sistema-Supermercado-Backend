085040f00266bbb16c7d15dedef2b8657754bf29
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-08-28 14:28:55 -0500

    Se creo una clase para la configuracion de cada entidad

---
+++ DataBaseFirst/Contexts/Configuration/DetalleVentaConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using DataBaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataBaseFirst.Contexts.Configuration
+{
+    public class DetalleVentaConfiguration : IEntityTypeConfiguration<DetalleVentum>
+    {
+        public void Configure(EntityTypeBuilder<DetalleVentum> builder)
+        {
+            builder.HasKey(e => e.IdDetalleVenta).HasName("PK__DETALLE___49DABA0CBFC2BF98");
+
+            builder.ToTable("DETALLE_VENTA");
+
+            builder.Property(e => e.IdDetalleVenta).HasColumnName("ID_DETALLE_VENTA");
+            builder.Property(e => e.Cantidad).HasColumnName("CANTIDAD");
+            builder.Property(e => e.Descuento)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("DESCUENTO");
+            builder.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("FECHA_REGISTRO");
+            builder.Property(e => e.IdProducto).HasColumnName("ID_PRODUCTO");
+            builder.Property(e => e.IdVenta).HasColumnName("ID_VENTA");
+            builder.Property(e => e.PrecioVenta)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("PRECIO_VENTA");
+            builder.Property(e => e.Subtotal)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("SUBTOTAL");
+
+            builder.HasOne(d => d.IdProductoNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdProducto)
+                .HasConstraintName("FK__DETALLE_V__ID_PR__2EDAF651");
+
+            builder.HasOne(d => d.IdVentaNavigation).WithMany(p => p.DetalleVenta)
+                .HasForeignKey(d => d.IdVenta)
+                .HasConstraintName("FK__DETALLE_V__ID_VE__2DE6D218");
+        }
+    }
+}
---
+++ DataBaseFirst/Contexts/Configuration/OfertaConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using DataBaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataBaseFirst.Contexts.Configuration
+{
+    public class OfertaConfiguration : IEntityTypeConfiguration<Ofertum>
+    {
+        public void Configure(EntityTypeBuilder<Ofertum> builder)
+        {
+            builder.HasKey(e => e.Id_Oferta).HasName("PK__OFERTA__D1CAC4475441B3C5");
+
+            builder.ToTable("OFERTA");
+
+            builder.Property(e => e.Id_Oferta).HasColumnName("ID_OFERTA");
+            builder.Property(e => e.Codigo)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("CODIGO");
+            builder.Property(e => e.Descripcion)
+                .HasMaxLength(250)
+                .IsUnicode(false)
+                .HasColumnName("DESCRIPCION");
+            builder.Property(e => e.Descuento)
+                .HasDefaultValue(0m)
+                .HasColumnType("decimal(5, 2)")
+                .HasColumnName("DESCUENTO");
+            builder.Property(e => e.Estado).HasColumnName("ESTADO");
+            builder.Property(e => e.Fecha_Creacion)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime")
+                .HasColumnName("FECHA_CREACION");
+            builder.Property(e => e.Fecha_Fin).HasColumnName("FECHA_FIN");
+            builder.Property(e => e.Fecha_Inicio).HasColumnName("FECHA_INICIO");
+            builder.Property(e => e.Id_Producto).HasColumnName("ID_PRODUCTO");
+            builder.Property(e => e.Nombre_Oferta)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("NOMBRE_OFERTA");
+
+            builder.HasOne(d => d.IdProductoNavigation).WithMany(p => p.Oferta)
+                .HasForeignKey(d => d.Id_Producto)
+                .HasConstraintName("FK__OFERTA__ID_PRODU__0A9D95DB");
+        }
+    }
+}
