92c7bea884836c0f2a7679f102ed50e9a57df3dc
AbrahamzzZ <hermanosfarfan@gmail.com>
2025-08-29 23:56:51 -0500

    Se agregaron las validaciones de acuerdo a las entidades que hay

---
+++ DataBaseFirst/Services/Validators/NegocioValidator.cs
@@ -0,0 +1,18 @@
+﻿using DataBaseFirst.Models;
+using FluentValidation;
+using Utilities.Shared;
+
+namespace DataBaseFirst.Services.Validators
+{
+    public class NegocioValidator : AbstractValidator<Negocio>
+    {
+        public NegocioValidator()
+        {
+            RuleFor(n => n.Nombre).NotEmpty().WithMessage(Mensajes.MESSAGE_EMPTY).MaximumLength(50).WithMessage(Mensajes.MESSAGE_MIN_LENGTH);
+            RuleFor(n => n.Direccion).NotEmpty().WithMessage(Mensajes.MESSAGE_EMPTY).MaximumLength(200).WithMessage(Mensajes.MESSAGE_MIN_LENGTH);
+            RuleFor(n => n.Ruc).NotEmpty().WithMessage(Mensajes.MESSAGE_EMPTY).Matches(@"^\d{13}$").WithMessage("El RUC deben contener exactamente 13 dígitos numéricos");
+            RuleFor(n => n.Telefono).NotEmpty().WithMessage(Mensajes.MESSAGE_EMPTY).Matches(@"^\d{10}$").WithMessage(Mensajes.MESSAGE_PHONE);
+            RuleFor(n => n.Correo_Electronico).NotEmpty().WithMessage(Mensajes.MESSAGE_EMPTY).EmailAddress().WithMessage(Mensajes.MESSAGE_EMAIL);
+        }
+    }
+}
