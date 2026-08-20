fbd605d98be138c1ed73925b146194488218d62e
Abraham <hermanosfarfan@gmail.com>
2026-08-20 17:13:20 -0500

    Se creo una nueva clase para el historial de precios de los productos

---
+++ Domain/Models/HistorialPrecioProducto.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models
+{
+    public class HistorialPrecioProducto
+    {
+        public int Id_Historial { get; set; }
+        public int Id_Producto { get; set; }
+        public decimal? Precio_Compra_Anterior { get; set; }
+        public decimal? Precio_Compra_Nuevo { get; set; }
+        public decimal? Precio_Venta_Anterior { get; set; }
+        public decimal? Precio_Venta_Nuevo { get; set; }
+        public string? Usuario_Modificacion { get; set; }
+        public DateTime Fecha_Cambio { get; set; }
+        public virtual Producto IdProductonavitaion { get; set; } = null!;
+    }
+}
